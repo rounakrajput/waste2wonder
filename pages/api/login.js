63122a5837a964a069b95886e2de561d6bb5f7ed
Rounak001 <rounaksingh2779@gmail.com>
2024-05-03 20:09:37 +0530

    created login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,6 @@
 import connectDB from "@/middleware/connectDB";
 import User from "@/Models/User";
+import JWT from "jsonwebtoken";
 
 const handler = async (req, res) => {
   if (req.method !== "POST") {
@@ -8,28 +9,28 @@ const handler = async (req, res) => {
   }
 
   const { fname, lname, email, password } = req.body ?? {};
-  
+
   // Check if all required fields are provided
-  if (!fname || !lname || !email || !password) {
-    res.status(403).send({ success: false, message: "Please fill up all details!" });
+  if (!email || !password) {
+    res
+      .status(403)
+      .send({ success: false, message: "Please fill up all details!" });
     return; // Added return to exit the function
   }
 
   try {
     const userExists = await User.findOne({ email });
-
-    if (userExists) {
-      return res.status(409).send({ success: false, message: "User already exists!" });
+    if (!userExists) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User not registered" });
     }
 
-    const newUser = await User.create({
-      fName: fname,
-      lName: lname,
-      email,
-      password,
-    });
-
-    res.status(201).send({ success: true, message: "User registered successfully!" });
+    if (password === userExists?.password) {
+        const token = JWT.sign({userExists},process.env.JWT_SECRET)
+      return res.status(200).send({ success: true, userExists,token });
+    }
+    res.status(401).send({ success: false, message: "invalid Credentials" });
   } catch (error) {
     // Handle any potential errors
     console.error("Error:", error);
